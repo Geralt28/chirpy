af6cf2bfa89421d15085b80ebb46b8649dea7944
Geralt <przemyslaw26@interia.pl>
2025-02-17 16:30:26 +0100

    Lekcja 1.11

--- main.go
+++ main.go
@@ -5,18 +5,31 @@ import (
 	"net/http"
 )
 
+func readinessHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8") //ustawianie headera odpowiedzi
+	res.WriteHeader(http.StatusOK)                                // ustawianie status code
+	res.Write([]byte("OK"))                                       //pisanie body odpowiedzi - musi byc jako Write([]byte) (int, error)
+
+}
+
 func main() {
 	mux := http.NewServeMux()
+	filePath := "/home/geralt/workspace/github.com/Geralt28/chirpy"
+	port := "8080"
+
+	mux.HandleFunc("/healthz", readinessHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePath+"/app"))))
+
+	//mux.Handle("/", http.FileServer(http.Dir(filePath)))
 
 	server := &http.Server{
-		Addr:    ":8080", // Bind to port 8080
-		Handler: mux,     // mux as a handler
+		Addr:    ":" + port, // Bind to port 8080
+		Handler: mux,        // mux as a handler
 	}
-
-	mux.Handle("/", http.FileServer(http.Dir("/home/geralt/workspace/github.com/Geralt28/chirpy")))
 
 	// Start the server
 	fmt.Println("Starting server on http://localhost:8080")
+	fmt.Printf("Serving files from %s on port: %s\n", filePath, port)
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error starting server:", err)
