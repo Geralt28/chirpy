352b63c1d8c6be4caf75fb3fa86f49494cca5bff
Geralt <przemyslaw26@interia.pl>
2025-02-17 18:35:13 +0100

    Lekcja 2.1

--- main.go
+++ main.go
@@ -3,12 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-func readinessHandler(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "text/plain; charset=utf-8") //ustawianie headera odpowiedzi
-	res.WriteHeader(http.StatusOK)                                // ustawianie status code
-	res.Write([]byte("OK"))                                       //pisanie body odpowiedzi - musi byc jako Write([]byte) (int, error)
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) numberServerHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		hits := cfg.fileserverHits.Load()
+		w.Write([]byte(fmt.Sprintf("Hits: %v\n", hits)))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetServerHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func readinessHandler(w http.ResponseWriter, rr *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") //ustawianie headera odpowiedzi
+	w.WriteHeader(http.StatusOK)                                // ustawianie status code
+	w.Write([]byte("OK"))                                       //pisanie body odpowiedzi - musi byc jako Write([]byte) (int, error)
 
 }
 
@@ -17,10 +46,12 @@ func main() {
 	filePath := "/home/geralt/workspace/github.com/Geralt28/chirpy"
 	port := "8080"
 
+	//mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePath+"/app"))))
 	mux.HandleFunc("/healthz", readinessHandler)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePath+"/app"))))
-
-	//mux.Handle("/", http.FileServer(http.Dir(filePath)))
+	cfg := &apiConfig{}
+	mux.Handle("/metrics/", cfg.numberServerHits(http.StripPrefix("/metrics", http.FileServer(http.Dir(filePath+"/metrics")))))
+	mux.Handle("/reset/", cfg.resetServerHits(http.StripPrefix("/reset", http.FileServer(http.Dir(filePath+"/reset")))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePath+"/app")))))
 
 	server := &http.Server{
 		Addr:    ":" + port, // Bind to port 8080
